28c9ed245e7be7bf18d1bfe4e0af0fecfa937f37
kumkum03-hub <kumkumkamthan2003@gmail.com>
2025-08-03 23:27:57 +0530

    Update homepage.js

--- homepage.js
+++ homepage.js
@@ -9,18 +9,35 @@
 
   let currentOfferIndex = 0;
 
-  function scrollOffer(direction) {
-    const wrapper = document.getElementById("offerWrapper");
-    const banners = wrapper.querySelectorAll(".offer-banner");
-  
-    currentOfferIndex += direction;
-  
-    if (currentOfferIndex < 0) currentOfferIndex = banners.length - 1;
-    if (currentOfferIndex >= banners.length) currentOfferIndex = 0;
-  
-    wrapper.scrollTo({
-      left: banners[currentOfferIndex].offsetLeft,
-      behavior: "smooth",
-    });
-  }
+function scrollOffer(direction) {
+  const wrapper = document.getElementById("offerWrapper");
+  const banners = wrapper.querySelectorAll(".offer-banner");
+
+  currentOfferIndex += direction;
+
+  if (currentOfferIndex < 0) currentOfferIndex = banners.length - 1;
+  if (currentOfferIndex >= banners.length) currentOfferIndex = 0;
+
+  const scrollAmount = wrapper.clientWidth * currentOfferIndex;
+
+  wrapper.scrollTo({
+    left: scrollAmount,
+    behavior: "smooth",
+  });
+}
+
+function scroll4(direction) {
+  const wrapper = document.getElementById("subContainer1");
+  const banners = wrapper.querySelectorAll(".card4");
+
+  currentOfferIndex += direction;
+
+  const scrollAmount = wrapper.clientWidth * currentOfferIndex;
+
+  wrapper.scrollTo({
+    left: scrollAmount,
+    behavior: "smooth",
+  });
+}
+
   
